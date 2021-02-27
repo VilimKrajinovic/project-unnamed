b7c71a40d60ade0854f4bbeeb1dafc3feac9a0db
Vilim <vilim.krajinovic@gmail.com>
2021-02-27 16:04:26 +0100

    Improved camera controlls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,14 +10,17 @@ public class PlayerMovement : MonoBehaviour {
     private float forwardAngle;
     private Vector3 forwardDirection;
 
-    // Update is called once per frame
     void Update() {
         Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
 
         forwardAngle = GetForwardAngle();
         forwardDirection = CalculateForwardDirection(forwardAngle);
-        ChangeDirectionToForwardAngle(ref moveDirection, forwardAngle);
-        MoveTowards(moveDirection);
+        if (moveDirection.magnitude > 0.1) {
+            ChangeDirectionToForwardAngle(ref moveDirection, forwardAngle);
+            Debug.DrawRay(transform.position, CalculateForwardDirection(forwardAngle), Color.green);
+            Debug.DrawRay(transform.position, moveDirection, Color.red);
+            MoveTowards(moveDirection);
+        }
     }
     private float GetForwardAngle() {
         if (IsRightMouseButtonHeld()) {
@@ -29,9 +32,10 @@ public class PlayerMovement : MonoBehaviour {
         return Quaternion.AngleAxis(forwardAngle, Vector3.up) * Vector3.forward;
     }
     private void ChangeDirectionToForwardAngle(ref Vector3 direction, float forwardAngle) {
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, forwardAngle, ref turnSmoothVelocity, 0.1f);
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
         direction = Quaternion.AngleAxis(forwardAngle, Vector3.up) * direction;
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, 0.1f);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
     }
 
     private void MoveTowards(Vector3 direction) {
