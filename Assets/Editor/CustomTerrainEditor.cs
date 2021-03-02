65c1ec89b93a359ad0a7556f2d4f8a17369d55d5
Vilim <vilim.krajinovic@gmail.com>
2021-03-02 22:03:51 +0100

    Voronoi

--- Assets/Editor/CustomTerrainEditor.cs
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -21,10 +21,19 @@ public class CustomTerrainEditor : Editor {
     SerializedProperty perlinOctaves;
     SerializedProperty perlinPersistance;
     SerializedProperty perlinHeightScale;
+    GUITableState perlinParametersTable;
+    SerializedProperty perlinParameters;
+    SerializedProperty voronoiPeakCount;
+    SerializedProperty voronoiFalloff;
+    SerializedProperty voronoiDropoff;
+    SerializedProperty voronoiMinHeight;
+    SerializedProperty voronoiMaxHeight;
 
     bool showRandom = false;
     bool showLoadHeights = false;
     bool showPerlin = false;
+    bool showMultiplePerlin = false;
+    bool showVoronoi = false;
     private void OnEnable() {
         shouldReset = serializedObject.FindProperty("shouldReset");
         randomHeightRange = serializedObject.FindProperty("randomHeightRange");
@@ -37,6 +46,13 @@ public class CustomTerrainEditor : Editor {
         perlinOctaves = serializedObject.FindProperty("perlinOctaves");
         perlinPersistance = serializedObject.FindProperty("perlinPersistance");
         perlinHeightScale = serializedObject.FindProperty("perlinHeightScale");
+        perlinParametersTable = new GUITableState("perlinParameterTable");
+        perlinParameters = serializedObject.FindProperty("perlinParameters");
+        voronoiPeakCount = serializedObject.FindProperty("voronoiPeakCount");
+        voronoiFalloff = serializedObject.FindProperty("voronoiFalloff");
+        voronoiDropoff = serializedObject.FindProperty("voronoiDropoff");
+        voronoiMinHeight = serializedObject.FindProperty("voronoiMinHeight");
+        voronoiMaxHeight = serializedObject.FindProperty("voronoiMaxHeight");
     }
 
     public override void OnInspectorGUI() {
@@ -73,15 +89,50 @@ public class CustomTerrainEditor : Editor {
                     Fields = new List<Field> {
                         new Field { Property = perlinXScale, Min = 0, Max = 1, SliderType = SliderType.FLOAT },
                         new Field { Property = perlinYScale, Min = 0, Max = 1, SliderType = SliderType.FLOAT },
-                        new Field { Property = perlinOffsetX, Min = 0, Max = 10000, SliderType = SliderType.INT },
-                        new Field { Property = perlinOffsetY, Min = 0, Max = 10000, SliderType = SliderType.INT },
-                        new Field { Property = perlinOctaves, Min = 1, Max = 10, SliderType = SliderType.INT },
+                        new Field { Property = perlinOffsetX, Min = 0, Max = 10000 },
+                        new Field { Property = perlinOffsetY, Min = 0, Max = 10000 },
+                        new Field { Property = perlinOctaves, Min = 1, Max = 10 },
                         new Field { Property = perlinPersistance, Min = 0.1f, Max = 10, SliderType = SliderType.FLOAT },
                         new Field { Property = perlinHeightScale, Min = 0, Max = 1, SliderType = SliderType.FLOAT },
                     },
                     Buttons = new Dictionary<string, Action> { { "Generate perlin terrain", terrain.Perlin } }
             });
         }
+
+        showMultiplePerlin = EditorGUILayout.Foldout(showMultiplePerlin, "Multiple perlin");
+        if (showMultiplePerlin) {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Multiple perlin noise", EditorStyles.boldLabel);
+            perlinParametersTable = GUITableLayout.DrawTable(perlinParametersTable, perlinParameters);
+            GUILayout.Space(20);
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("+")) {
+                terrain.AddNewPerlin();
+            }
+            if (GUILayout.Button("-")) {
+                terrain.RemovePerlin();
+            }
+            EditorGUILayout.EndHorizontal();
+            if (GUILayout.Button("Apply multiple perlin")) {
+                terrain.MultiplePerlinTerrain();
+            }
+        }
+
+        showVoronoi = EditorGUILayout.Foldout(showVoronoi, "Voronoi");
+        if (showVoronoi) {
+            Render(new Context {
+                Label = "Voronai noise",
+                    Fields = new List<Field> {
+                        new Field { Property = voronoiPeakCount, Min = 0, Max = 10 },
+                        new Field { Property = voronoiFalloff, Min = 0, Max = 10f, SliderType = SliderType.FLOAT },
+                        new Field { Property = voronoiDropoff, Min = 0, Max = 10f, SliderType = SliderType.FLOAT },
+                        new Field { Property = voronoiMaxHeight, Min = 0, Max = 1f, SliderType = SliderType.FLOAT },
+                        new Field { Property = voronoiMinHeight, Min = 0, Max = 1f, SliderType = SliderType.FLOAT },
+                    },
+                    Buttons = new Dictionary<string, Action> { { "Generate voronoi", terrain.Voronoi } }
+            });
+        }
+
         RenderDivider();
         if (GUILayout.Button("Zero out terrain")) {
             terrain.ZeroOut();
@@ -92,20 +143,22 @@ public class CustomTerrainEditor : Editor {
     private void Render(Context context) {
         RenderDivider();
         GUILayout.Label(context.Label, EditorStyles.boldLabel);
-        foreach (Field field in context.Fields) {
-            if (field.Max != null && field.Min != null) {
-                switch (field.SliderType) {
-                    case SliderType.FLOAT:
-                        EditorGUILayout.Slider(field.Property, field.Min.Value, field.Max.Value);
-                        break;
-                    case SliderType.INT:
-                        EditorGUILayout.IntSlider(field.Property, (int) field.Min.Value, (int) field.Max.Value);
-                        break;
-                    default:
-                        break;
+        if (context.Fields != null) {
+            foreach (Field field in context.Fields) {
+                if (field.Max != null && field.Min != null) {
+                    switch (field.SliderType) {
+                        case SliderType.FLOAT:
+                            EditorGUILayout.Slider(field.Property, field.Min.Value, field.Max.Value);
+                            break;
+                        case SliderType.INT:
+                            EditorGUILayout.IntSlider(field.Property, (int) field.Min.Value, (int) field.Max.Value);
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    EditorGUILayout.PropertyField(field.Property);
                 }
-            } else {
-                EditorGUILayout.PropertyField(field.Property);
             }
         }
         foreach (KeyValuePair<string, Action> entry in context.Buttons) {
@@ -127,7 +180,7 @@ public class CustomTerrainEditor : Editor {
 
     class Field {
         public SerializedProperty Property { get; set; }
-        public SliderType? SliderType { get; set; }
+        public SliderType? SliderType { get; set; } = CustomTerrainEditor.SliderType.INT;
         public float? Max { get; set; }
         public float? Min { get; set; }
     }
--- Assets/Scripts/CustomTerrain.cs
+++ Assets/Scripts/CustomTerrain.cs
@@ -13,6 +13,7 @@ public class CustomTerrain : MonoBehaviour {
 
     public bool shouldReset = true;
 
+    //SINGLE PERLIN
     public float perlinXScale = 0.01f;
     public float perlinYScale = 0.01f;
     public int perlinOffsetX = 0;
@@ -21,16 +22,60 @@ public class CustomTerrain : MonoBehaviour {
     public float perlinPersistance = 8;
     public float perlinHeightScale = 0.09f;
 
+    //MULTIPLE PERLIN
+    [System.Serializable]
+    public class PerlinParameters {
+        public float perlinXScale = 0.01f;
+        public float perlinYScale = 0.01f;
+        public int perlinOffsetX = 0;
+        public int perlinOffsetY = 0;
+        public int perlinOctaves = 3;
+        public float perlinPersistance = 8;
+        public float perlinHeightScale = 0.09f;
+        public bool remove = false;
+    }
+    public List<PerlinParameters> perlinParameters = new List<PerlinParameters>() {
+        new PerlinParameters()
+    };
+
+    // VORONOI
+    public int voronoiPeakCount = 1;
+    public float voronoiFalloff = 0.2f;
+    public float voronoiDropoff = 0.6f;
+    public float voronoiMinHeight = 0;
+    public float voronoiMaxHeight = 0.4f;
+
     public Terrain terrain;
     public TerrainData terrainData;
 
-    private float[, ] GetHeightMap() {
-        if (shouldReset) {
-            return new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
-        }
-        return terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
-    }
+    public void Voronoi() {
+        float[, ] heightMap = GetHeightMap();
 
+        for (int i = 0; i < voronoiPeakCount; i++) {
+            Vector3 peak = new Vector3(
+                Random.Range(0, terrainData.heightmapResolution),
+                Random.Range(voronoiMinHeight, voronoiMaxHeight),
+                Random.Range(0, terrainData.heightmapResolution)
+            );
+
+            heightMap[(int) peak.x, (int) peak.z] += peak.y;
+
+            Vector2 peakLocation = new Vector2(peak.x, peak.z);
+            float maxDistance = Vector2.Distance(new Vector2(0, 0), new Vector2(terrainData.heightmapResolution, terrainData.heightmapResolution));
+
+            for (int y = 0; y < terrainData.heightmapResolution; y++) {
+                for (int x = 0; x < terrainData.heightmapResolution; x++) {
+                    if (!(x == peak.x && y == peak.y)) {
+                        float distanceToPeak = Vector2.Distance(peakLocation, new Vector2(x, y)) / maxDistance;
+                        float height = peak.y - distanceToPeak * voronoiFalloff - Mathf.Pow(distanceToPeak, voronoiDropoff);
+                        if (heightMap[x, y] < height)
+                            heightMap[x, y] = height;
+                    }
+                }
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
     public void Perlin() {
         float[, ] heightMap = GetHeightMap();
         for (int y = 0; y < terrainData.heightmapResolution; y++) {
@@ -40,6 +85,36 @@ public class CustomTerrain : MonoBehaviour {
         }
         terrainData.SetHeights(0, 0, heightMap);
     }
+
+    public void MultiplePerlinTerrain() {
+        float[, ] heightMap = GetHeightMap();
+        for (int y = 0; y < terrainData.heightmapResolution; y++) {
+            for (int x = 0; x < terrainData.heightmapResolution; x++) {
+                foreach (PerlinParameters p in perlinParameters) {
+                    heightMap[x, y] += Utils.fBM((x + p.perlinOffsetX) * p.perlinXScale, (y + p.perlinOffsetY) * p.perlinYScale, p.perlinOctaves, p.perlinPersistance) * p.perlinHeightScale;
+                }
+            }
+        }
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    public void AddNewPerlin() {
+        perlinParameters.Add(new PerlinParameters());
+    }
+
+    public void RemovePerlin() {
+        List<PerlinParameters> keptPerlinParameters = new List<PerlinParameters>();
+        for (int i = 0; i < perlinParameters.Count; i++) {
+            if (!perlinParameters[i].remove) {
+                keptPerlinParameters.Add(perlinParameters[i]);
+            }
+        }
+        if (keptPerlinParameters.Count == 0) {
+            keptPerlinParameters.Add(perlinParameters[0]);
+        }
+        perlinParameters = keptPerlinParameters;
+    }
+
     public void RandomTerrain() {
         float[, ] heightMap = GetHeightMap();
         for (int x = 0; x < terrainData.heightmapResolution; x++) {
@@ -68,6 +143,13 @@ public class CustomTerrain : MonoBehaviour {
             }
         }
         terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    private float[, ] GetHeightMap() {
+        if (shouldReset) {
+            return new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
+        }
+        return terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
     }
 
     private void OnEnable() {
