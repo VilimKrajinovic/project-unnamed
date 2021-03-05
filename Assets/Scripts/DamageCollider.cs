d4a58dc6b625404b679f096674dbbbb69fa3458d
Vilim <vilim.krajinovic@gmail.com>
2021-03-05 20:04:13 +0100

    Weapon collision and damage

---
+++ Assets/Scripts/DamageCollider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VK {
+    public class DamageCollider : MonoBehaviour {
+        Collider[] damageColliders;
+
+        public int currentWeaponDamage;
+        private void Awake() {
+            damageColliders = GetComponentsInChildren<Collider>();
+            foreach (Collider c in damageColliders) {
+                c.gameObject.SetActive(true);
+                c.isTrigger = true;
+                c.enabled = false;
+            }
+        }
+
+        public void EnableDamageCollider() {
+            foreach (Collider c in damageColliders) {
+                c.enabled = true;
+            }
+        }
+
+        public void DisableDamageCollider() {
+            foreach (Collider c in damageColliders) {
+                c.enabled = false;
+            }
+        }
+
+        private void OnTriggerEnter(Collider collision) {
+            if (collision.tag == "Player") {
+                PlayerStats playerStats = collision.GetComponent<PlayerStats>();
+                if (playerStats != null) {
+                    playerStats.TakeDamage(currentWeaponDamage);
+                }
+            }
+            if (collision.tag == "Enemy") {
+                EnemyStats enemyStats = collision.GetComponent<EnemyStats>();
+                if (enemyStats != null) {
+                    enemyStats.TakeDamage(currentWeaponDamage);
+                }
+            }
+        }
+
+    }
+}
--- Assets/Scripts/WeaponSlotManager.cs
+++ Assets/Scripts/WeaponSlotManager.cs
@@ -7,6 +7,9 @@ namespace VK {
         WeaponHolderSlot leftHandSlot;
         WeaponHolderSlot rightHandSlot;
 
+        DamageCollider leftHandDamageCollider;
+        DamageCollider righHandtDamageCollider;
+
         private void Awake() {
             WeaponHolderSlot[] weaponHolderSlots = GetComponentsInChildren<WeaponHolderSlot>();
             foreach (WeaponHolderSlot weaponSlot in weaponHolderSlots) {
@@ -21,9 +24,36 @@ namespace VK {
         public void LoadWeapnOnSlot(WeaponItem weaponItem, bool isLeft) {
             if (isLeft) {
                 leftHandSlot.LoadWeaponModel(weaponItem);
+                LoadLeftWeaponDamageCollider();
             } else {
                 rightHandSlot.LoadWeaponModel(weaponItem);
+                LoadRightWeaponDamageCollider();
             }
         }
+
+        #region Handle Weapon's Damage collider
+        private void LoadLeftWeaponDamageCollider() {
+            leftHandDamageCollider = leftHandSlot.currentWeaponModel.GetComponentInChildren<DamageCollider>();
+        }
+
+        private void LoadRightWeaponDamageCollider() {
+            righHandtDamageCollider = rightHandSlot.currentWeaponModel.GetComponentInChildren<DamageCollider>();
+        }
+
+        public void OpenLefttDamageCollider() {
+            leftHandDamageCollider.EnableDamageCollider();
+        }
+        public void OpenRightDamageCollider() {
+            righHandtDamageCollider.EnableDamageCollider();
+        }
+
+        public void CloseLefttDamageCollider() {
+            leftHandDamageCollider.DisableDamageCollider();
+        }
+        public void CloseRightDamageCollider() {
+            righHandtDamageCollider.DisableDamageCollider();
+        }
+        #endregion
+
     }
 }
